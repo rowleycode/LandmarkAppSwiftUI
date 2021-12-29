c2af0c30157409d720be71c0659285084848e125
Caleb Rowley <caleb@stonespot.com>
2021-12-28 16:47:47 -0800

    The Master Comment

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 
+//This is the new master branch. If a future version of me is reading this feel free to delete this comment.
+
 struct ContentView: View {
     var body: some View {
         LandmarkList()
